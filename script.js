51cd155188864770620d6752689a9b1603386a5c
Belaz4x4 <belaz4x4@yandex.ru>
2023-05-09 13:31:08 +0300

    common task completed

--- script.js
+++ script.js
@@ -11,28 +11,61 @@ let money = +prompt('Ваш месячный доход в рублях?'),
     mission = 200000, 
     period = 8;
 
-
-console.log(typeof money, typeof income, typeof deposit);
-console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-console.log(addExpenses.split(' '));
-
-let budgetMonth = (money + income - amount1 - amount2);
-console.log(budgetMonth);
-
-let timeToMission = Math.ceil(mission / budgetMonth);
-console.log(timeToMission);
-
-let budgetDay = Math.floor(budgetMonth / 30)
-console.log(budgetDay);
-
-if (budgetDay > 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay <= 1200 && budgetDay >= 600 ) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что то пошло не так');
+function getStatusIncome(budgetDay) {
+    if (budgetDay > 1200) {
+        return'У вас высокий уровень дохода';
+    } else if (budgetDay <= 1200 && budgetDay >= 600 ) {
+        return'У вас средний уровень дохода';
+    } else if (budgetDay < 600) {
+        return'К сожалению у вас уровень дохода ниже среднего';
+    } else {
+        return'Что то пошло не так';
 }
+}
+
+function getExpensesMonth(...expenses) {
+    let sum = 0;
+    expenses.forEach((item) => {
+        sum += item
+    })
+    return sum
+}
+
+function showTypeOf(...items) {
+    items.forEach((item) => {
+        console.log(typeof item);
+    })
+}
+
+// const getAccumulatedMonth = function (income, ...expenses) {
+//     let accumulated = income
+//     expenses.forEach((item) => {
+//         accumulated -= item
+//     })
+//     return accumulated
+// }
+
+const getAccumulatedMonth = function (income, expenses) {
+    return income - expenses
+};
+
+const getTargetMonth = (mission, accumulated) => {
+    return Math.ceil(mission / accumulated)
+}
+
+showTypeOf(money, income, deposit);
+
+let monthExpenses = getExpensesMonth(amount1, amount2);
+console.log(`Расходы за  месяц: ${monthExpenses}`);
+
+console.log(`Возможные расходы: ${addExpenses.split(', ')}`);
+
+let accumulatedMonth = getAccumulatedMonth(money, monthExpenses);
+let monthToTarget = getTargetMonth(mission, accumulatedMonth);
+console.log(`Месяцев до цели: ${monthToTarget}`);
+
+let budgetDay = Math.floor(accumulatedMonth / 30)
+console.log(`Дневной бюджет: ${budgetDay}`);
+
+let statusIncome = getStatusIncome(budgetDay)
+console.log(statusIncome);
