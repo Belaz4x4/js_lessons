68fdb9399b75decfc386a85541c833f1e4afef3a
Belaz4x4 <belaz4x4@yandex.ru>
2023-05-15 08:12:26 +0300

    advanced task complete

--- script.js
+++ script.js
@@ -1,74 +1,90 @@
 'use strict';
 
 function game(minNumber, maxNumber, tryesAmount) {
-    let agreement = confirm(`Я загадал число от ${minNumber} до ${maxNumber}. Хотите его угадать?`);
+    let agreement = confirm(`Я загадал число от ${minNumber} до ${maxNumber}. Хотите его угадать?`),
+        hiddenNumber,
+        roundsCount;
 
-
-    if (agreement) {
-        let hiddenNumber    = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber,
-            roundsCount     = tryesAmount;
-
-        alert(`У вас ${tryesAmount} попыток`)
-
-        function gameRound() {
-            if (roundsCount === 0) 
-            {
-                playMore = confirm('Попытки закончились, хотите сыграть еще?');
-                if(playMore) 
-                {
-                    return game(minNumber, maxNumber, tryesAmount) // почему я не могу вернуть родительскую функцию, если ее имя есть в глобальной области видимости?
-                } else 
-                {
-                    alert('Игра окончена. Спасибо за игру.')
-                }
-                
-            } else
-            {
-                let usersAnswer = getUsersAnswer(`Введите целое число от ${minNumber} до ${maxNumber}.\nЕсли не хотите продолжать, нажмите "отмена".`, minNumber, maxNumber); // как сделать перенос строки в коде без переноса строки в модальном окне?
-                                                
-                if (usersAnswer === 'exit') 
-                {
-                    alert('Игра окончена. Спасибо за игру.')
-                } else if (usersAnswer === hiddenNumber) 
-                {
-                    alert('Поздравляю, вы угадали!!!')
-                } else if (usersAnswer < hiddenNumber) 
-                {
-                    roundsCount--
-                    alert(`Загаданное число больше. Осталось попыток: ${tryesAmount}`)
-                    return gameRound()
-                } else
-                {
-                    roundsCount--
-                    alert(`Загаданное число меньше. Осталось попыток: ${tryesAmount}`)
-                    return gameRound()
-                }
+    function getUsersAnswer(message) {
+        let answer = prompt(message);
+    
+        if (answer === null) {
+            return 'exit';
+        } else {
+            answer = +answer.trim();
+            if (isNaN(answer) || answer < minNumber || answer > maxNumber) {
+                alert('Введено неверное значение!')
+                return getUsersAnswer(message, minNumber, maxNumber)
+            } else {
+                return answer
             }
         }
-       
+    }
+
+    function startGame() {
+        hiddenNumber    = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber,
+        roundsCount     = tryesAmount;
+
+        alert(`У вас ${tryesAmount} попыток`)
+    }
+
+    function gameRound() {
+        if (roundsCount === 0) 
+        {
+            playAgain('Попытки закончились. Хотите сыграть еще?')
+                        
+        } else
+        {
+            let usersAnswer = getUsersAnswer(`Введите целое число от ${minNumber} до ${maxNumber}.\nЕсли не хотите продолжать, нажмите "отмена".`, minNumber, maxNumber); // как сделать перенос строки в коде без переноса строки в модальном окне?
+                                            
+            if (usersAnswer === 'exit') 
+            {
+                alert('Игра окончена. Спасибо за игру.')
+            } else if (usersAnswer === hiddenNumber) 
+            {
+                playAgain('Поздравляю, вы угадали!!! Хотите сыграть еще?')
+            } else if (usersAnswer < hiddenNumber) 
+            {
+                nextRound(`Загаданное число больше. Осталось попыток: ${roundsCount - 1}`)
+            } else
+            {
+                nextRound(`Загаданное число меньше. Осталось попыток: ${roundsCount - 1}`)
+            }
+        }
+    }
+
+    function playAgain(message) {
+        let playMore = confirm(message);
+        if(playMore) 
+        {
+            startGame();
+            return gameRound()
+        } else 
+        {
+            alert('Игра окончена. Спасибо за игру.')
+        }
+    }
+
+    function nextRound(message) {
+        --roundsCount
+        alert(message)
+        return gameRound()
+    }
+
+    // старт исполнения
+
+    if (agreement) {
+        startGame()
+              
         return gameRound
     }
 }
 
-function getUsersAnswer(message, minNumber, lastNumer) {
-    let answer = prompt(message);
 
-    if (answer === null) {
-        return 'exit';
-    } else {
-        answer = +answer.trim();
-        if (isNaN(answer) || answer < minNumber || answer > lastNumer) {
-            alert('Введено неверное значение!')
-            return getUsersAnswer(message, minNumber, lastNumer)
-        } else {
-            return answer
-        }
-    }
-}
 
-let firstGame = game(1, 10, 5);
+// let firstGame = game(1, 10, 3);
 
-firstGame()
+// firstGame()
 
 let secondGame = game(1, 100, 10);
 
