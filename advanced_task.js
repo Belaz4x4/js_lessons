33fd559b31bb2796ead41241d83c81451334f5cd
Belaz4x4 <belaz4x4@yandex.ru>
2023-05-18 12:45:35 +0300

    advanced task complete

--- advanced_task.js
+++ advanced_task.js
@@ -1,21 +1,81 @@
 'use strict';
 
 let date        = new Date,
-    currentDay   = date.getDay(),
-    cnt = 1,
-    week = ['Monday', 'Tuesday', 'Wednesday', 'Thurshday', 'Friday', 'Saturday', 'Sunday'];
+    week        = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+    monthInYear = ['янраря', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']; 
+  
+function  outputDate(date){
+    let dayOfWeek           = week[date.getDay()],
+        day                 =  date.getDate(),
+        month               = monthInYear[date.getMonth()],
+        year                = date.getFullYear(),
+        hour                = date.getHours(),
+        HourDeclension      =  getMaleDeclension(hour),
+        minute              = date.getMinutes(),
+        minuteDeclension    = getFemaleDeclension(minute),
+        second              = Math.round(date.getSeconds()),
+        secondDeclension    = getFemaleDeclension(second);
 
-for (let day of week) {
-    if (cnt === currentDay) 
-    {
-        console.log(day.bold())
-    } else if (cnt >= 6) 
-    {
-        console.log(day.italics())
-    }else {
-        console.log(day);
+    return `Сегодоня ${dayOfWeek}, ${day} ${month} ${year} года, ${hour} час${HourDeclension} ${minute} минут${minuteDeclension} ${second} секунд${secondDeclension}`;
+}
+
+function  outputShortDate(date){
+    let time = {
+        day: String(date.getDate()),
+        month: String(date.getMonth() + 1),
+        year: String(date.getFullYear()),
+        hour: String(date.getHours()),
+        minute: String(date.getMinutes()),
+        second: String(date.getSeconds()),
+    }
+        
+    for (let key in time) {
+        if (time[key].length < 2) time[key] = '0' + time[key];
     }
 
-    ++cnt
+    return `${time.day}.${time.month}.${time.year} - ${time.hour}:${time.minute}:${time.second}'`;
 }
-  
+
+function getMaleDeclension(number) {
+    number = +number
+    if ((number > 4 && number <= 20) || number === 0) 
+    {
+        return 'ов'
+    }
+    number = +String(number).slice(-1)
+    if (number === 1)
+    {
+        return ''
+    } else if (number > 1 && number <= 4) 
+    {
+        return 'а'
+    } else 
+    {
+        return 'ов'
+    }
+}
+
+function getFemaleDeclension(number) {
+    number = +number
+    if ((number > 4 && number <= 20) || number === 0) 
+    {
+        return ''
+    }
+    number = +String(number).slice(-1)
+    if (number === 1)
+    {
+        return 'а'
+    } else if (number > 1 && number <= 4) 
+    {
+        return 'ы'
+    } else 
+    {
+        return ''
+    }
+}
+
+let timeLong = document.querySelector('.time__long');
+let timeShort = document.querySelector('.time__short');  
+timeLong.innerHTML = outputDate(date)
+timeShort.innerText = outputShortDate(date)
+
